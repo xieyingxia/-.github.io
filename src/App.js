a73c8d76143ed90b8056eca231cfdad8826898cc
xieyingxia <xieyingxia_1003@yahoo.co.jp>
2020-03-17 17:54:53 +0900

    Signed-off-by: xieyingxia <xieyingxia_1003@yahoo.co.jp>

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import intl from 'react-intl-universal'
+import IndexPageHeader from '../src/pages/indexPage/indexPageHeader'
+import AppRouter from './router/AppRouter'
+class App extends React.Component {
+    render () {
+        return (           
+            <Router> 
+                <IndexPageHeader/>  
+               {
+                  AppRouter.map((item,index)=>{
+                    if(item.isShow){
+                        if(item.exact){
+                            return (
+                                <Route
+                                key={index}
+                                exact
+                                path={item.path}
+                                render={props=>(
+                                    <item.component {...props}/>
+                                )}
+                                />
+                            )
+                        }else{
+                            return (
+                                <Route
+                                key={index}
+                                path={item.path}
+                                render={props=>(
+                                    <item.component {...props}/>
+                                )}
+                                />
+                            )
+                        }
+                    }
+                  })   
+                }
+        </Router> 
+     
+        )
+    }
+}
+export default App
